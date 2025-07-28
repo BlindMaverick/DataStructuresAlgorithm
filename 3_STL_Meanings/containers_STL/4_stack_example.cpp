62b8dec4f9e92eee04444e93d29836f25613c963
Vishruth V Shelkey <vishruthvshelkey@Vishruths-MacBook-Pro.local>
2025-07-28 19:38:01 +0530

    updated Stack STL

--- 3_STL_Meanings/containers_STL/4_stack_example.cpp
+++ 3_STL_Meanings/containers_STL/4_stack_example.cpp
@@ -1,6 +1,24 @@
 #include <bits/stdc++.h>
-using namespace std
+using namespace std;
 
 void explainStack()
 {
+    stack<int> st;
+    st.push(1);    // {1}
+    st.push(2);    // {2,1}
+    st.push(3);    // {3,2,1}
+    st.emplace(4); //{4,3,2,1}
+
+    cout << st.top(); // Prints : 4
+
+    st.pop(); // popped item : 4, stack st = {3,2,1}
+
+    cout << st.top(); // Prints : 3
+
+    cout << st.size(); // Prints : 4
+
+    cout << st.empty();
+
+    stack<int> st1, st2;
+    st1.swap(st2);
 }
